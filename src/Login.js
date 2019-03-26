c6f20113f91c5d45e881982c0f4d984350afb749
unknown <kevinma91@yahoo.com>
2019-03-26 00:50:52 -0700

    added validation error UI

--- src/Login.js
+++ src/Login.js
@@ -12,20 +12,21 @@ export default class Login extends Component {
       password: "",
       passwordConfirmation: ""
     };
+
+    this.handleChange = this.handleChange.bind(this); // bind method to class instance, so it is accessible when called in form
   }
 
-  handleChange(event) {
+  handleChange = event => {
     event.preventDefault();
     const name = event.target.name;
     const value = event.target.value;
+    console.log("name: " + name);
+    console.log("value: " + value);
 
-    console.log(name);
-    console.log(value);
+    this.setState({
+      [name] : value
+    });
 
-    // this.setState({
-    //   name : value
-    // });
-    console.log(event);
   }
 
   render () {
@@ -35,16 +36,51 @@ export default class Login extends Component {
           <div className="col-auto">
             <h4 className="text-center">Please log in.</h4>
             <form className="form-group">
-              <input type="text" name="fName" onChange={this.handleChange} className="form-control loginForm--input" placeholder="First Name" value={this.state.fName} required/>
-              <input type="text" name="lName" onChange={this.handleChange} className="form-control loginForm--input" placeholder="Last Name" value={this.state.lName} required/>
-              <input type="text" name="email" onChange={this.handleChange} className="form-control loginForm--input" placeholder="Email" value={this.state.email} required/>
-              <input type="text" name="password" onChange={this.handleChange} className="form-control loginForm--input" placeholder="Password" value={this.state.password} required/>
-              <input type="password" onChange={this.handleChange} name="passwordConfirmation" className="form-control loginForm--input" placeholder="Password Confirmation" value={this.state.passwordConfirmation} required/>
-              <input type="password" name="passwordConf" className="hidden" placeholder="Password Confirmation" required/>
-              <div className="loginForm--buttons justify-content-center">
-                <button className="btn btn-primary" onClick={this.handleChange}>Login</button>
-                <button className="btn btn-danger">Register</button>
-              </div>
+
+                <div className="fieldGrp">
+                  <div className="invalid-feedback display-block">
+                  Please choose a username.
+                  </div>
+                  <input type="text" name="fName" onChange={this.handleChange} className="form-control is-invalid loginForm--input" placeholder="First Name" value={this.state.fName} required/>
+                </div>
+
+                <div className="fieldGrp">
+                  <div className="invalid-feedback display-block">
+                  Please choose a username.
+                  </div>
+                  <input type="text" name="lName" onChange={this.handleChange} className="form-control is-invalid loginForm--input" placeholder="Last Name" value={this.state.lName} required/>
+                </div>
+
+                <div className="fieldGrp">
+                  <div className="invalid-feedback display-block">
+                  Please choose a username.
+                  </div>
+                  <input type="text" name="email" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Email" value={this.state.email} required/>
+                </div>
+
+                <div className="fieldGrp">
+                  <div className="invalid-feedback display-block">
+                  Please choose a username.
+                  </div>
+                  <input type="password" name="password" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Password" value={this.state.password} required/>
+                </div>
+
+                <div className="fieldGrp">
+                  <div className="invalid-feedback display-block">
+                  Please choose a username.
+                  </div>
+                  <input type="password" name="passwordConfirmation" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Password Confirmation" value={this.state.passwordConfirmation} required/>
+                </div>
+
+                <input type="password" name="passwordConf" className="hidden" placeholder="Password Confirmation"/>
+                <div className="center-content m-1 mt-4">
+                  <button className="btn btn-danger">Register</button>
+                </div>
+
+                <div className="loginForm--buttons justify-content-center m-1 mt-3">
+                  <button className="btn btn-primary" onClick={this.handleChange}>Login</button>
+                  <button className="btn btn-danger">Register</button>
+                </div>
             </form>
           </div>
         </div>
