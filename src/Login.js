4d776d512b6973ee66ec6e1418d58da7f4b8e8bd
unknown <kevinma91@yahoo.com>
2019-03-28 15:01:07 -0700

    added front end DOM validation

--- src/Login.js
+++ src/Login.js
@@ -10,7 +10,13 @@ export default class Login extends Component {
       lName: "",
       email: "",
       password: "",
-      passwordConfirmation: ""
+      passwordConfirmation: "",
+      fNameValid: false,
+      lNameValid: false,
+      emailValid: false,
+      passwordValid: false,
+      passwordMatch: false,
+      testRegEmail: "test@test.com" // Used as a sample test registration email
     };
 
     this.handleChange = this.handleChange.bind(this); // bind method to class instance, so it is accessible when called in form
@@ -18,15 +24,37 @@ export default class Login extends Component {
 
   handleChange = event => {
     event.preventDefault();
-    const name = event.target.name;
+    const formName = event.target.name;
     const value = event.target.value;
-    console.log("name: " + name);
-    console.log("value: " + value);
+    // console.log("name: " + formName);
+    // console.log("value: " + value);
 
     this.setState({
-      [name] : value
+      [formName] : value
     });
 
+    this.validateInput();
+  }
+
+  validateInput = () => {
+    console.log("validation now");
+    // let validRegistration = true;
+    //
+    // if (formName === "email" && value === this.state.testRegEmail) { // Check if email is already used (testRegEmail)
+    //   console.log("error");
+    // }
+    //
+    // if (this.state.password === this.state.passwordConfirmation) { // Check is passwords match
+    //   console.log("password error");
+    // }
+    //
+    // if (this.state.password.length < 7) { // Check password length
+    //   console.log("password too short");
+    // }
+    //
+    // if (validRegistration) {
+    //   // send POST request with form input to backend to save to the DB
+    // }
   }
 
   render () {
@@ -38,38 +66,38 @@ export default class Login extends Component {
             <form className="form-group">
 
                 <div className="fieldGrp">
-                  <div className="invalid-feedback display-block">
+                  <div className="invalid-feedback">
                   Please choose a username.
                   </div>
-                  <input type="text" name="fName" onChange={this.handleChange} className="form-control is-invalid loginForm--input" placeholder="First Name" value={this.state.fName} required/>
+                  <input type="text" name="fName" onChange={this.handleChange} className="form-control loginForm--input" placeholder="First Name" value={this.state.fName} pattern="^[a-zA-Z]+$" required/>
                 </div>
 
                 <div className="fieldGrp">
                   <div className="invalid-feedback display-block">
                   Please choose a username.
                   </div>
-                  <input type="text" name="lName" onChange={this.handleChange} className="form-control is-invalid loginForm--input" placeholder="Last Name" value={this.state.lName} required/>
+                  <input type="text" name="lName" onChange={this.handleChange} className="form-control loginForm--input" placeholder="Last Name" value={this.state.lName} pattern="^[a-zA-Z][A-Za-z0-9\. ]*$" required/>
                 </div>
 
                 <div className="fieldGrp">
-                  <div className="invalid-feedback display-block">
+                  <div className="invalid-feedback">
                   Please choose a username.
                   </div>
-                  <input type="text" name="email" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Email" value={this.state.email} required/>
+                  <input type="text" name="email" onChange={this.handleChange} className="form-control loginForm--input" placeholder="Email" value={this.state.email} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
                 </div>
 
                 <div className="fieldGrp">
-                  <div className="invalid-feedback display-block">
+                  <div className="invalid-feedback">
                   Please choose a username.
                   </div>
-                  <input type="password" name="password" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Password" value={this.state.password} required/>
+                  <input type="password" name="password" onChange={this.handleChange} className="form-control loginForm--input" placeholder="Password" minLength="8" maxLength="15" value={this.state.password} required/>
                 </div>
 
                 <div className="fieldGrp">
-                  <div className="invalid-feedback display-block">
+                  <div className="invalid-feedback">
                   Please choose a username.
                   </div>
-                  <input type="password" name="passwordConfirmation" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Password Confirmation" value={this.state.passwordConfirmation} required/>
+                  <input type="password" name="passwordConfirmation" onChange={this.handleChange} className="form-control loginForm--input" placeholder="Password Confirmation" minLength="8" maxLength="15" value={this.state.passwordConfirmation} required/>
                 </div>
 
                 <input type="password" name="passwordConf" className="hidden" placeholder="Password Confirmation"/>
@@ -88,3 +116,58 @@ export default class Login extends Component {
     )
   }
 }
+
+// <div className="container">
+//   <div className="row justify-content-center loginForm">
+//     <div className="col-auto">
+//       <h4 className="text-center">Please log in.</h4>
+//       <form className="form-group">
+//
+//           <div className="fieldGrp">
+//             <div className="invalid-feedback display-block">
+//             Please choose a username.
+//             </div>
+//             <input type="text" name="fName" onChange={this.handleChange} className="form-control is-invalid loginForm--input" placeholder="First Name" value={this.state.fName} pattern="^[a-zA-Z]+$" required/>
+//           </div>
+//
+//           <div className="fieldGrp">
+//             <div className="invalid-feedback display-block">
+//             Please choose a username.
+//             </div>
+//             <input type="text" name="lName" onChange={this.handleChange} className="form-control is-invalid loginForm--input" placeholder="Last Name" value={this.state.lName} pattern="^[a-zA-Z][A-Za-z0-9\. ]*$" required/>
+//           </div>
+//
+//           <div className="fieldGrp">
+//             <div className="invalid-feedback display-block">
+//             Please choose a username.
+//             </div>
+//             <input type="text" name="email" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Email" value={this.state.email} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
+//           </div>
+//
+//           <div className="fieldGrp">
+//             <div className="invalid-feedback display-block">
+//             Please choose a username.
+//             </div>
+//             <input type="password" name="password" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Password" minLength="8" maxLength="15" value={this.state.password} required/>
+//           </div>
+//
+//           <div className="fieldGrp">
+//             <div className="invalid-feedback display-block">
+//             Please choose a username.
+//             </div>
+//             <input type="password" name="passwordConfirmation" onChange={this.handleChange} className="form-control is-valid loginForm--input" placeholder="Password Confirmation" minLength="8" maxLength="15" value={this.state.passwordConfirmation} required/>
+//           </div>
+//
+//           <input type="password" name="passwordConf" className="hidden" placeholder="Password Confirmation"/>
+//           <div className="center-content m-1 mt-4">
+//             <button className="btn btn-danger">Register</button>
+//           </div>
+//
+//           <div className="loginForm--buttons justify-content-center m-1 mt-3">
+//             <button className="btn btn-primary" onClick={this.handleChange}>Login</button>
+//             <button className="btn btn-danger">Register</button>
+//           </div>
+//       </form>
+//     </div>
+//   </div>
+// </div>
